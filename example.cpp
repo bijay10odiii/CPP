238758e31b15e40e1049259d02ad2b02e25f4fee
Bijay Satyal <bijaysatyal@Bijay-MacBook-Pro.local>
2025-09-19 09:17:33 +0300

    my first commit

---
+++ example.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<map>
+#include<tuple>
+
+using namespace std; 
+
+int main(){
+    string name= "Bijay";
+    vector<int> marks{85, 90, 78};
+    map<string, int> ageMap;
+    ageMap["Alice"] = 25;
+    ageMap["Bob"] = 30;
+
+    tuple<string, int, bool> person= {"Bijay", 20, true};
+    cout<< "Name: "<< name << endl;
+    cout<< ">First Score: " << score[0] << endl;
+    cout<< ">Bob's Age: " << ageMap["Bob"] << endl;
+    cout<< "Tuple name:" << get<0>(person) << endl;
+    return 0;
+    
+}
